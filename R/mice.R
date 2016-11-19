f0c557afcd2b269cefd0aef595ce7c20a419d4c1
yilong zhang <elong0527@gmail.com>
2016-11-19 15:26:09 -0500

    Add files via upload

---
+++ R/mice.R
@@ -0,0 +1,45 @@
+#' A simulated mice data
+#'
+#' A dataset containing longitudinal mice OTU table.
+#'
+#' @format A data frame with 240 rows and 37 variables:
+#' \describe{
+#'   \item{id}{mouse ID}
+#'   \item{time}{observation time}
+#'   \item{group}{treatment group}
+#'   \item{OTU.1}{relative abundance of a OTU}
+#'   \item{OTU.2}{relative abundance of a OTU}
+#'   \item{OTU.3}{relative abundance of a OTU}
+#'   \item{OTU.4}{relative abundance of a OTU}
+#'   \item{OTU.5}{relative abundance of a OTU}
+#'   \item{OTU.6}{relative abundance of a OTU}
+#'   \item{OTU.7}{relative abundance of a OTU}
+#'   \item{OTU.8}{relative abundance of a OTU}
+#'   \item{OTU.9}{relative abundance of a OTU}
+#'   \item{OTU.10}{relative abundance of a OTU}
+#'   \item{OTU.11}{relative abundance of a OTU}
+#'   \item{OTU.12}{relative abundance of a OTU}
+#'   \item{OTU.13}{relative abundance of a OTU}
+#'   \item{OTU.14}{relative abundance of a OTU}
+#'   \item{OTU.15}{relative abundance of a OTU}
+#'   \item{OTU.16}{relative abundance of a OTU}
+#'   \item{OTU.17}{relative abundance of a OTU}
+#'   \item{OTU.18}{relative abundance of a OTU}
+#'   \item{OTU.19}{relative abundance of a OTU}
+#'   \item{OTU.20}{relative abundance of a OTU}
+#'   \item{OTU.21}{relative abundance of a OTU}
+#'   \item{OTU.22}{relative abundance of a OTU}
+#'   \item{OTU.23}{relative abundance of a OTU}
+#'   \item{OTU.24}{relative abundance of a OTU}
+#'   \item{OTU.25}{relative abundance of a OTU}
+#'   \item{OTU.26}{relative abundance of a OTU}
+#'   \item{OTU.27}{relative abundance of a OTU}
+#'   \item{OTU.28}{relative abundance of a OTU}
+#'   \item{OTU.29}{relative abundance of a OTU}
+#'   \item{OTU.30}{relative abundance of a OTU}
+#'   \item{OTU.31}{relative abundance of a OTU}
+#'   \item{OTU.32}{relative abundance of a OTU}
+#'   \item{OTU.33}{relative abundance of a OTU}
+#'   \item{OTU.34}{relative abundance of a OTU}
+#' }
+"mice"
